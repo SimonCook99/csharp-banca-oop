994ce6bba7fb3410680d85a6e72dbfaf43c7ee8c
Simone Mauro <simon.mauro@virgilio.it>
2022-07-01 17:20:54 +0200

    Menù completato, gestione totale prestiti e lista associata a un cliente

--- Banca.cs
+++ Banca.cs
@@ -17,6 +17,7 @@ namespace csharp_banca_oop
             this.clienti.Add(cliente);
         }
 
+        //funzione per stampare la lista dei clienti
         public void stampaClienti(){
             foreach (Cliente cliente in this.clienti){
                 Console.WriteLine($"Nome: {cliente.Nome} \t Cognome: {cliente.Cognome} \t CF: {cliente.CF}");
@@ -24,13 +25,15 @@ namespace csharp_banca_oop
             Console.WriteLine();
         }
 
-        public void stampaPrestiti(){
-            foreach (Prestito prestito in this.prestiti){
+        //funzione per stampare una lista di prestiti (o tutti i prestiti della banca, o quelli relativi a un solo cliente)
+        public void stampaPrestiti(List<Prestito> prestiti){
+            foreach (Prestito prestito in prestiti){
                 Console.WriteLine($"Prestito n. {prestito.ID} intestato a: {prestito.intestatario.Nome} {prestito.intestatario.Cognome} con un ammontare di {prestito.ammontare}€ da pagare in {prestito.rata} rate");
             }
             Console.WriteLine();
         }
 
+        //funzione per la modifica di un cliente, dato il codice fiscale identificativo
         public void ModificaCliente(string codiceFiscaleVecchioCliente, Cliente nuovoCliente){
             foreach (Cliente cliente in this.clienti){
                 if(cliente.CF == codiceFiscaleVecchioCliente){
@@ -44,6 +47,7 @@ namespace csharp_banca_oop
             
         }
 
+        //funzione di ricerca di un cliente, utilizzato sia per l'aggiunta di prestiti, che per vedere le informazioni dei prestiti di un cliente
         public Cliente ricercaCliente(string codiceFiscaleDaCercare){
             foreach (Cliente cliente in this.clienti){
                 if(cliente.CF == codiceFiscaleDaCercare){
@@ -57,6 +61,7 @@ namespace csharp_banca_oop
 
         }
 
+        //aggiunta prestito, associandolo ad un determinato cliente come intestatario
         public void aggiungiPrestito(Cliente intestatario, int ammontare, int rata, DateTime inizio, DateTime fine){
             if (this.clienti.Contains(intestatario)){
                 this.prestiti.Add(new Prestito(intestatario, ammontare, rata, inizio, fine));
@@ -64,6 +69,30 @@ namespace csharp_banca_oop
             else{
                 Console.WriteLine("Spiacente, l'utente non è registrato alla piattaforma");
             }
+        }
+
+        //funzione per ricavare la sottolista di prestiti di un solo cliente
+        public List<Prestito> RicavaPrestitiCliente(Cliente intestatario){
+            List<Prestito> prestitiCliente = new List<Prestito>();
+            foreach (Prestito prestito in this.prestiti){
+                if (prestito.intestatario == intestatario){
+                    prestitiCliente.Add(prestito);
+                }
+            }
+            return prestitiCliente;
+        }
+
+        //funzione per ricavare la somma di tutti i prestiti associati a un cliente
+        public double sommaPrestitiDiUnCLiente(Cliente cliente){
+            double totale = 0;
+
+            foreach (Prestito prestito in this.prestiti){
+                if(prestito.intestatario == cliente){
+                    totale += prestito.ammontare;
+                }
+            }
+
+            return totale;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using csharp_banca_oop;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
 
 Banca b = new Banca();
 b.Nome = "Intesa San Paolo";
@@ -8,8 +11,11 @@ Cliente Pippo = new Cliente("Pippo", "test", "pptst998redqdsd2581", 1300);
 b.clienti.Add(Simone);
 b.clienti.Add(Pippo);
 
+b.aggiungiPrestito(Simone, 2000, 10, new DateTime(2022, 8, 5), new DateTime(2023, 6, 21));
+b.aggiungiPrestito(Simone, 15000, 36, new DateTime(2022, 10, 12), new DateTime(2024, 11, 23));
+
 b.stampaClienti(); //stampo prima la lista clienti con i clienti predefiniti
-b.stampaPrestiti();
+b.stampaPrestiti(b.prestiti);
 
 int counter = 0;
 while(counter < 4){
@@ -17,9 +23,12 @@ while(counter < 4){
     Console.WriteLine("1. aggiungi cliente");
     Console.WriteLine("2. modifica cliente");
     Console.WriteLine("3. aggiungi prestito");
+    Console.WriteLine("4. Vedere l'ammontare totale dei prestiti di un cliente");
+    Console.WriteLine("5. Vedere tutti i prestiti associati a un cliente");
+
+
     int option = int.Parse(Console.ReadLine());
-    switch (option)
-    {
+    switch (option){
         case 1:
             Console.WriteLine("Inserisci il nome:");
             string nome = Console.ReadLine();
@@ -53,10 +62,35 @@ while(counter < 4){
                 string CFSearch = Console.ReadLine();
                 intestatario = b.ricercaCliente(CFSearch);
             }
-            b.aggiungiPrestito(intestatario, 2000, 15, new DateTime(2022, 8, 5), new DateTime(2023, 6, 21));
-            b.stampaPrestiti();
+            Console.WriteLine("Inserisci l'ammontare del prestito");
+            int ammontare = int.Parse(Console.ReadLine());
+            b.aggiungiPrestito(intestatario, ammontare, 15, new DateTime(2022, 8, 5), new DateTime(2023, 6, 21));
+            b.stampaPrestiti(b.prestiti);
             counter++;
             break;
+        case 4:
+            Cliente trovato = null;
+            while (trovato == null){
+                Console.WriteLine("Inserisci il codice fiscale del cliente di cui vuoi sapere il totale");
+                string CFSearch = Console.ReadLine();
+                trovato = b.ricercaCliente(CFSearch);
+            }
+            double totale = b.sommaPrestitiDiUnCLiente(trovato);
+            Console.WriteLine("L'ammontare totale di tutti i prestiti del seguente cliente è " + totale + "€");
+            break;
+
+        case 5:
+            Cliente cliente = null;
+            while (cliente == null){
+                Console.WriteLine("Inserisci il codice fiscale del cliente di cui vuoi vedere i prestiti");
+                string CFSearch = Console.ReadLine();
+                cliente = b.ricercaCliente(CFSearch);
+            }
+
+            List<Prestito> prestitoList = b.RicavaPrestitiCliente(cliente);
+            b.stampaPrestiti(prestitoList);
+            break;
+
     }
 
 }
@@ -75,4 +109,4 @@ while(counter < 4){
 //b.aggiungiPrestito(Simone, 2500, 20, new DateTime(2022, 8, 5), new DateTime(2023, 6, 21));
 //b.aggiungiPrestito(Pippo, 5000, 52, new DateTime(2022, 8, 5), new DateTime(2023, 6, 21));
 
-b.stampaPrestiti();
+b.stampaPrestiti(b.prestiti);
